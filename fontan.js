e5d14f6c67bc97458da91c24b3f04223b6e569e7
Oguzok <defenzdec@gmail.com>
2025-06-04 13:08:45 +0300

    Add files via upload

---
+++ fontan.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+ const buttons = document.querySelectorAll('.product button');
+
+ buttons.forEach(button => {
+     button.addEventListener('click', function() {
+         alert('Детальная информация о ' + this.parentElement.querySelector('h3').innerText);
+     });
+ });
+});
