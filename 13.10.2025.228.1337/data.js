fa804656db6c171ad1bba948f31290198218654a
Oguzok <defenzdec@gmail.com>
2025-10-13 13:40:17 +0300

    Add files via upload

---
+++ 13.10.2025.228.1337/data.js
@@ -0,0 +1,21 @@
+const dataInfo = 
+[
+  {
+    "id": "b7b",
+    "url": "https://cdn2.thecatapi.com/images/b7b.jpg",
+    "width": 500,
+    "height": 334
+  },
+   {
+    "id": "st1MHwYbH",
+    "url": "https://cdn2.thecatapi.com/images/st1MHwYbH.jpg",
+    "width": 3840,
+    "height": 2160
+  },
+  {
+    "id": "9aj",
+    "url": "https://cdn2.thecatapi.com/images/9aj.jpg",
+    "width": 600,
+    "height": 450
+  }
+]
---
+++ 13.10.2025.228.1337/samosval.js
@@ -0,0 +1,17 @@
+const nasaPics = [
+  {
+    title: "Milky Way Galaxy",
+    explanation: "This is the Milky Way as seen on a clear night.",
+    url: "https://i.pinimg.com/736x/ba/c1/31/bac131e09fee9661fdb35e61bf096a71.jpg"
+  },
+  {
+    title: "Mars Surface",
+    explanation: "Photo of the Martian surface taken by a rover.",
+    url: "https://avatars.mds.yandex.net/i?id=4ba6feefeeffda65c574336f22144a01_l-5194967-images-thumbs&n=13"
+  },
+  {
+    title: "Saturn Rings",
+    explanation: "Saturn’s iconic rings imaged by Cassini spacecraft.",
+    url: "https://avatars.mds.yandex.net/i?id=29fb08c8dc40801bb1e3c91d65d1a71778a54b8f-10385132-images-thumbs&n=13"
+  }
+]
